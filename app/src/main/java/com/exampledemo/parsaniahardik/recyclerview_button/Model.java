7c0e4c4d754b55f696f0f852670764a9734596a5
Maggie Sellers <msellers@smith.edu>
2018-08-28 09:55:58 -0400

    Initial commit

---
+++ app/src/main/java/com/exampledemo/parsaniahardik/recyclerview_button/Model.java
@@ -0,0 +1,26 @@
+package com.exampledemo.parsaniahardik.recyclerview_button;
+
+/**
+ * Created by hardik on 9/1/17.
+ */
+public class Model {
+
+    private int number;
+    private String fruit;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+}
